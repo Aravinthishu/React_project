6bb379be291f4dcf58b39e71fd25db33c29b64a9
Aravinthishu <145826939+Aravinthishu@users.noreply.github.com>
2024-06-19 22:34:12 -0700

    Update Gallery.jsx

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import gallery05 from "../../images/portfolio_5.png";
 import gallery01 from "../../images/portfolio_1.png";
 import gallery02 from "../../images/MAIN.png";
 import gallery03 from "../../images/portfolio_3.jpg";
 import gallery04 from "../../images/portfolio_4.png";
-import gallery05 from "../../images/portfolio_5.png";
+
 import "./gallery.css";
 
 const Gallery = () => {
   const imageLinks = [
-    "https://aravinthan-portfolio.vercel.app/",
+    "https://gym-frontend-self.vercel.app/",
+    "https://www.figma.com/proto/2Rw7G0PPW0hy8BclM7N5WZ?node-id=0-1&t=Lfk0N2VPlhf6Zywp-6",
     "https://photography-tawny-mu.vercel.app/portfolio",
     "https://www.figma.com/proto/2Rw7G0PPW0hy8BclM7N5WZ?node-id=0-1&t=Lfk0N2VPlhf6Zywp-6",
     "https://aravinthan-portfolio.vercel.app/",
-    "https://gym-frontend-self.vercel.app/",
   ];
 
   const scrollRef = React.useRef(null);
